552e52bd6bb5832a5a6e835cb82d2de9784d36a3
dungnguyenquy <nguyenquydung2002@gmail.com>
2024-05-10 15:09:55 +0700

    firsttime

---
+++ app/src/main/java/com/example/movieapp/Activities/IntroActivity.java
@@ -0,0 +1,29 @@
+package com.example.movieapp.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.movieapp.databinding.ActivityIntroBinding;
+
+public class IntroActivity extends AppCompatActivity {
+    ActivityIntroBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding=ActivityIntroBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.startBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(IntroActivity.this, MainActivity.class));
+            }
+        });
+        Window w=getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+    }
+}
